caf6f38a6e257c6dc34710d984af5589bb775b36
xavier <samuelpedroso08@gmail.com>
2025-03-20 11:24:32 -0300

    Subindo ajustes no design das informações

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.css';
 
-function SearchBar() {
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+function SearchBar({onSearch}: SearchBarProps) {
+
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSearch(query);
+  }
+
   return (
-    <div className={styles.bar}>
-      <input className={styles.input} type="text" placeholder="Pesquise uma cidade ou bairro..." />
+    <form className={styles.bar} onClick={handleSubmit}>
+      <input 
+        className={styles.input} 
+        type="text" 
+        placeholder="Pesquise uma cidade ou bairro..." 
+        value={query} onChange={(e) => setQuery(e.target.value)} 
+      />
       <button className={styles.button} type="submit">
         <img className={styles.lupa} src={process.env.PUBLIC_URL + "/pesquisa.svg"} alt="Submit" />
       </button>
-    </div>
+    </form>
   );
 }
 
