615282c0bd50af747f22c5e7c45178d47730ae51
HeitorCarvalhoCampos <heitor.campos@germinare.org.br>
2025-03-23 15:31:24 -0300

    Barra de previsão semanal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar/SearchBar'; 
-import Information from './components/Information/Information';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Information from "./components/Information/Information";
+import WeekBar from "./components/WeekBar/WeekBar";
 
 interface Informacao {
   temperatura: string;
@@ -9,52 +10,124 @@ interface Informacao {
   bairro: string;
 }
 
-
+interface WeekInformation {
+  data: string;
+  high_temp: string;
+  low_temp: string;
+  max_temp: string;
+  min_temp: string;
+  condicao: string;
+  wind_speed: number;
+  humidity: number;
+  rain: number;
+}
 
 function App() {
   const apiKey = "1663760659498a69525148f4061c7d65";
+  const apiKey2 = "d8235fe0545e4dd8adf7186f2c647f73";
   const [informacoes, setInformacoes] = useState<Informacao[]>([]);
+  const [weekInformacoes, setWeekInformacoes] = useState<WeekInformation[]>([]);
 
   useEffect(() => {
-    if (informacoes.length === 0) { 
+    if (informacoes.length === 0) {
       obterLocalizacao();
     }
-  }, [informacoes]); 
+  }, [informacoes]);
 
-function obterLocalizacao() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
+  useEffect(() => {
+    if (weekInformacoes.length === 0) {
+      obterWeekInformacoes();
+    }
+  }, [weekInformacoes]);
 
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=pt`)
-        .then(response => response.json())
-        .then(weather => {
-          console.log(weather);
-          const novaInformacao: Informacao = {
-            temperatura: weather.main.temp + "°C",
-            condicao: weather.weather[0].description,
-            bairro: weather.name,
-          };
-          setInformacoes(prevInformacoes => [novaInformacao]);
-        })
-        .catch(error => console.error("Erro ao obter clima:", error));
-    }, (error) => {
-      console.error("Erro ao obter localização", error);
-    });
-  } else {
-    console.log("Geolocalização não disponível no navegador.");
+  function obterLocalizacao() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+
+          fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=pt`
+          )
+            .then((response) => response.json())
+            .then((weather) => {
+              console.log(weather);
+              const novaInformacao: Informacao = {
+                temperatura: weather.main.temp + "°C",
+                condicao: weather.weather[0].description,
+                bairro: weather.name,
+              };
+              setInformacoes((prevInformacoes) => [novaInformacao]);
+            })
+            .catch((error) => console.error("Erro ao obter clima:", error));
+        },
+        (error) => {
+          console.error("Erro ao obter localização", error);
+        }
+      );
+    } else {
+      console.log("Geolocalização não disponível no navegador.");
+    }
   }
-}
+
+  function obterWeekInformacoes() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+  
+          fetch(
+            `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${apiKey2}`
+          )
+            .then((response) => response.json())
+            .then((weather) => {
+              if (!weather.data || weather.data.length < 8) {
+                console.error("Dados insuficientes da previsão do tempo.");
+                return;
+              }
+  
+              const informacoes: WeekInformation[] = weather.data.map((dia: any) => ({
+                data: dia.datetime,
+                high_temp: dia.high_temp + "°C",
+                low_temp: dia.low_temp + "°C",
+                max_temp: dia.max_temp + "°C",
+                min_temp: dia.min_temp + "°C",
+                condicao: dia.weather.description,
+                wind_speed: dia.wind_spd,
+                humidity: dia.rh,
+                rain: dia.precip,
+              }));
+  
+              setWeekInformacoes(informacoes);
+            })
+            .catch((error) => console.error("Erro ao obter clima:", error));
+        },
+        (error) => {
+          console.error("Erro ao obter localização", error);
+        }
+      );
+    } else {
+      console.log("Geolocalização não disponível no navegador.");
+    }
+  }
 
   return (
     <div className="App">
       <div className="divLogo">
-        <img className='logo' src={process.env.PUBLIC_URL + "/logo-climorld.png"} alt="Mundo Clima" />
-        <SearchBar/>
+        <img
+          className="logo"
+          src={process.env.PUBLIC_URL + "/logo-climorld.png"}
+          alt="Mundo Clima"
+        />
+        <SearchBar />
       </div>
       <div className="divInformations">
         <Information informacoes={informacoes} />
+      </div>
+      <div className="divWeek">
+        <WeekBar weekInformations={weekInformacoes} />
       </div>
     </div>
   );
--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,32 +1,32 @@
-import App from '../../App'
-import styles from './Information.module.css'
+import App from "../../App";
+import styles from "./Information.module.css";
 
 interface Informacao {
-    temperatura: string;
-    condicao: string;
-    bairro: string;
-  }
-  
-  interface InformationProps {
-    informacoes: Informacao[];
-  }
+  temperatura: string;
+  condicao: string;
+  bairro: string;
+}
+
+interface InformationProps {
+  informacoes: Informacao[];
+}
 
 function Information({ informacoes }: InformationProps) {
-    return (
-        <div className={styles.divInfos}>
-        {informacoes.length !== 0 ? (
-          informacoes.map((informacao, index) => (
-            <div key={index} className={styles.divInfo}>
-                <p>Bairro: {informacao.bairro}</p>
-                <p>Temperatura: {informacao.temperatura}</p>
-                <p>Condição: {informacao.condicao}</p>
-            </div>
-          ))
-        ) : (
-          <p>Ative a localização</p>
-        )}
-      </div>
-    );
-  }
-  
+  return (
+    <div className={styles.divInfos}>
+      {informacoes.length !== 0 ? (
+        informacoes.map((informacao, index) => (
+          <div key={index} className={styles.divInfo}>
+            <p>Bairro: {informacao.bairro}</p>
+            <p>Temperatura: {informacao.temperatura}</p>
+            <p>Condição: {informacao.condicao}</p>
+          </div>
+        ))
+      ) : (
+        <p>Ative a localização</p>
+      )}
+    </div>
+  );
+}
+
 export default Information;
